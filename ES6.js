a02aa4b2ecbf1bad608c1460993965731771944d
Ramaj Johnson <rjohnson3795@gmail.com>
2025-05-02 12:40:33 -0500

    Template Literal

--- ES6.js
+++ ES6.js
@@ -1,7 +1,14 @@
 //Practicing with ES6 Fundamentals
-//1. Template Literals
+
+/*1. Template Literals: Makes it easier to create strings with embedded expressions. 
+Used for multi-line strings and string interpolation. 
+Dynamically create strings using variables.*/
+// Example 1: Using Template Literals
 const firstName = 'John';
 const lastName = 'Doe';
-const fullNameConcat = firstName + ' ' + lastName;
-const fullName = `${firstName} ${lastName}`;
-console.log(fullName); // John Doe
+const fullNameConcat = firstName + ' ' + lastName; //Before ES6
+const fullNameLiteral = `${firstName} ${lastName}`; //After ES6
+console.log(fullNameLiteral); // John Doe
+console.log(fullNameConcat); // John Doe
+
+
