2e6357517b145c16a96669b628a98b452542d3bb
Ramaj Johnson <rjohnson3795@gmail.com>
2025-05-02 12:36:32 -0500

    Template Literal

--- ES6.js
+++ ES6.js
@@ -1,1 +1,7 @@
 //Practicing with ES6 Fundamentals
+//1. Template Literals
+const firstName = 'John';
+const lastName = 'Doe';
+const fullNameConcat = firstName + ' ' + lastName;
+const fullName = `${firstName} ${lastName}`;
+console.log(fullName); // John Doe
